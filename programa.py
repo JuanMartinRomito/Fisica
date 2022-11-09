af2617b9099ff4946865dd404ded824b2f7a1124
JuanMartinRomito <116213197+JuanMartinRomito@users.noreply.github.com>
2022-11-09 19:03:46 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,58 +1,71 @@
-#Parte 2 ejercicio 1 Emilia Ortega, Lucio Manitta y Juanmartin Romito
+#Importacion de la libreria necesaria para poder graficar
 import matplotlib.pyplot as plt
-
-repeticion=int(input("Cuantas veces queres que se repita el codigo (Para que el grafico funcione mejor, poner para que el codigo se repita mas de una vez): "))
-lista=[]
-x=[]
-y=[]
+#Le preguntamos al usuario cuantas veces se va a repetir el codigo
+repeticion=int(input("Cuantas veces queres que se repita el codigo: "))
+#Listas que llenaremos luego para hacer los graficos
+graf1x=[]
+graf1y=[]
+graf2y=[]
+graf2x=[]
+#Cantidad de veces que se va a repetir el codigo (el usuario lo elije)
 for i in range(0,repeticion):
     def variables():
-        #Pedir al usuario que quiere calcular
-        vel= float(input("ingrese la velocidad (m/s): "))
+        #El usuario ingresa los valores con los que luego vamos a hacer las cuentas
+        vel= float(input("ingrese la velocidad en m/s: "))
+        #Bucle para no dejar que ingrese velocidad en negativo
         while vel<0:
-          vel= float(input("noooo negativo noo ingrese la velocidad devuelta en pero en positivo: "))
+          vel= float(input("Por favor, ingrese la velocidad en positivo:"))
           if vel>0:
             break
-        cabal=float(input("ingrese la distancia del caballo hasta el auto(metros): "))
+        cabal=float(input("ingrese la distancia del objeto 1 hasta el objeto 2(en metros): "))
+        #Bucle para no dejar que el usuario ingrese la distancia en negativo
         while cabal<0:
-          cabal=float(input("noooooo negativo nooo ingrese la distancia del caballo hasta el auto devuelta (en metros y positivo): "))
+          cabal=float(input("Por favor, ingresar de nuevo la distancia entre los dos puntos (en metros y positivo): "))
           if cabal>0:
             break
-        fren= float(input("ingrese a que velocidad el auto desacelera(en negativo): "))
+        fren= float(input("ingrese a que aceleracion el objeto 1 desacelera(expresalo en negativo): "))
         return calculos(vel, cabal, fren)
     def calculos(vel, cabal, fren):
+        #Bucle para que el usuario no pueda poner la desaceleracion en positivo
         while fren>0:
             fren= float(input("NO se puede la desaceleracion numero positivo ponelo en negativo: "))
             if fren<0:
                 break
-        #Calculos con lo que haya puesto el usuario
+        #Calculos con los numeros que haya puesto el usuario anteriormente
         calc= 0-vel
         tie= calc/fren
         dist= 0+vel*tie+0.5*fren*(tie*tie)
+        #Resultados
         if dist>cabal:
-            print("Te tragaste a un caballo de frente pobre de el :(")
+            print("El objeto 1 choco al objeto 2")
             c = vel + (1/2)*(0-vel)
             c2 = cabal/c
             acel = (0-vel)/c2
-            print("nececitabas frenar a ", acel, "m/s2 para no chocarte")
-            print("lo chocaste en ",tie, "segundos")
-            lista.append(acel)
-            lista.append(tie)
+            print("El objeto 1 necesitaba frenar a ", acel, "m/s2 para no chocar y lo choco en",tie, "segundos")
         elif dist<=cabal:
             dist2=cabal-dist
-            print("te quedaste a",dist2, "metros de chocar al caballo")
-            print("Frenaste en ",tie, "segundos")
-            lista.append(dist2)
-            lista.append(tie)
-        y.append(tie)
-        x.append(dist)
+            print("Lo choco en",tie,"segundos y necesitaba frenar a",dist2,"m/s2 para no chocar")
+        #Agregado de los resultados a las listas de los graficos para poder graficar mas abajo
+        graf1y.append(tie)
+        graf1x.append(dist)
+        graf2x.append(vel)
+        graf2y.append(tie)
     variables()
-print("Los resultados fueron",lista)
-#Grafico
-width=0.1
-plt.bar(y,x, width=width, color="grey")
-plt.plot(y,x, color="blue")
-plt.ylabel("Distancia")
-plt.xlabel("Tiempo")
-plt.title("Distancia en funcion del tiempo tiempo")
+#Grafico 1 (Distancia en funcion del tiempo)
+width=0.5
+plt.figure(1)
+plt.bar(graf1y,graf1x, width=width, color="grey")
+plt.plot(graf1y, graf1x, color="black")
+plt.ylabel("Distancia (m)")
+plt.xlabel("Tiempo (s)")
+plt.title("Distancia sobre tiempo")
+#Grafico 2 (Velocidad en funcion del tiempo)
+plt.figure(2)
+width=2
+plt.scatter(graf2y,graf2x,color="blue")
+plt.plot(graf2y,graf2x, color="grey")
+plt.ylabel("Velocidad (m/s)")
+plt.xlabel("Tiempo (s)")
+plt.title("Velocidad en funcion del Tiempo del objeto 1")
 plt.show()
+#Fin :)
